95847442b10278d971521325ccdd50fe6b803ea7
Lgilat <luigilbla@alu.edu.gva.es>
2024-03-19 18:40:46 +0100

    nuevo intento

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'https://github.com/LGilat/SvelTareas',
   plugins: [svelte()],
 })
