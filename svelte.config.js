c36554f3a8844b597a72287b11c6d0a56f34c0b6
Lgilat <luigilbla@alu.edu.gva.es>
2024-03-19 18:16:49 +0100

    "nuevo intento de deploy"

--- svelte.config.js
+++ svelte.config.js
@@ -4,5 +4,19 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
+  kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
+    paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	},
   preprocess: vitePreprocess(),
 }
